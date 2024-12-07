e41d8e3b16e43fdbe58ed530c58166babf87b9bf
Chris Bradwell <chrisbradwell@Chriss-MacBook-Pro.local>
2024-12-07 11:42:50 +0000

    Basic NVIM setup Complete

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,37 +1,24 @@
-require 'core.options'
-require 'core.keymaps'
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
-end 
+end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-{
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-},
-{
-	"christoomey/vim-tmux-navigator",
-	lazy = false,
-},
-{
-    "baliestri/aura-theme",
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
-    end
-  }
-})
+
+local opts = {}
+
+require("vim-options")
+require("lazy").setup("plugins")
+
+
